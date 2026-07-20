4f96318f71cdd90a938971d0c6df54e19d40d43c
Qiwqiw-Alt <forgamefauzan10@gmail.com>
2026-07-20 17:38:23 +0700

    implement AppNavigation with NavHost and routes for Home, Add, Edit, and Detail screens

--- mobile-app/coba-todo/app/src/main/java/com/example/my_todo/core/route/AppNavigation.kt
+++ mobile-app/coba-todo/app/src/main/java/com/example/my_todo/core/route/AppNavigation.kt
@@ -1,4 +1,59 @@
 package com.example.my_todo.core.route
 
-class AppNavigation {
+import androidx.compose.runtime.Composable
+import androidx.navigation.compose.NavHost
+import androidx.navigation.compose.composable
+import androidx.navigation.compose.rememberNavController
+import androidx.navigation.toRoute
+import com.example.my_todo.feature.todo.presentation.AddScreen
+import com.example.my_todo.feature.todo.presentation.EditScreen
+import com.example.my_todo.feature.todo.presentation.DetailScreen
+import com.example.my_todo.feature.todo.presentation.HomepageScreen
+
+@Composable
+fun AppNavigation() {
+    val navController = rememberNavController()
+
+    NavHost(
+        navController = navController,
+        startDestination = HomeRoute
+    ) {
+        composable<HomeRoute> {
+            HomepageScreen(
+                onNavigateToAdd = {
+                    navController.navigate(AddRoute)
+                },
+                onNavigateToDetail = { taskId ->
+                    navController.navigate(DetailRoute(taskId))
+                },
+                onNavigateToEdit = { taskId ->
+                    navController.navigate((EditRoute(taskId)))
+                }
+            )
+        }
+
+        composable<AddRoute> {
+            AddScreen(onBack = { navController.popBackStack() } )
+        }
+
+        composable<EditRoute> { backStackEntry ->
+            val route = backStackEntry.toRoute<EditRoute>()
+
+            EditScreen(
+                currentIdTask = route.taskId,
+                onBack = { navController.popBackStack() },
+                onNavigateToDetail = { taskId -> navController.navigate(DetailRoute(taskId))}
+            )
+        }
+
+        composable<DetailRoute> { backStackEntry ->
+            val route = backStackEntry.toRoute<DetailRoute>()
+
+            DetailScreen(
+                currentIdTask = route.taskId,
+                onBack = { navController.popBackStack() },
+                onNavigateToEdit = { taskId -> navController.navigate(EditRoute(taskId))}
+            )
+        }
+    }
 }
