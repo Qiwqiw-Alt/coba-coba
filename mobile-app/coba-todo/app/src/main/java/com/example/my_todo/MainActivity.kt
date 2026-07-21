3e811d9d4a155ac4a87870919a0a45813f51e75f
Qiwqiw-Alt <forgamefauzan10@gmail.com>
2026-07-21 19:14:34 +0700

    implement splash screen in MainActivity

--- mobile-app/coba-todo/app/src/main/java/com/example/my_todo/MainActivity.kt
+++ mobile-app/coba-todo/app/src/main/java/com/example/my_todo/MainActivity.kt
@@ -4,13 +4,27 @@ import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
 import androidx.activity.enableEdgeToEdge
+import androidx.lifecycle.lifecycleScope
 import com.example.my_todo.core.route.AppNavigation
 import com.example.my_todo.ui.theme.My_todoTheme
+import androidx.core.splashscreen.SplashScreen.Companion.installSplashScreen
 import dagger.hilt.android.AndroidEntryPoint
+import kotlinx.coroutines.delay
+import kotlinx.coroutines.launch
 
 @AndroidEntryPoint
 class MainActivity : ComponentActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
+        val splashScreen = installSplashScreen()
+
+        var keepSplashScreen = true
+        splashScreen.setKeepOnScreenCondition { keepSplashScreen }
+
+        lifecycleScope.launch {
+            delay(1500)
+            keepSplashScreen = false
+        }
+
         super.onCreate(savedInstanceState)
         enableEdgeToEdge()
         setContent {
